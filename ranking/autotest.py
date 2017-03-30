5c8030e0b1770d7fe99d31c9cc74b53d994dd9d0
Yongyao Jiang <J.yongyao11@gmail.com>
2017-03-29 23:13:19 -0400

    Added visuals for both loss and accuracy

--- ranking/autotest.py
+++ ranking/autotest.py
@@ -2,6 +2,7 @@
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 # Train data with DL model
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
@@ -34,10 +35,11 @@ with open('/Users/yjiang/Dropbox/DLData/training_auto_new_only_query.csv', newli
     data_iter = csv.reader(csvfile, delimiter=',', quotechar='|')
     input_data_2 = [data for data in data_iter]
         
-train, test = train_test_split(input_data, test_size = 0.3)
+train, test = train_test_split(input_data, test_size = 0.1)
 test_data, test_labels = convert(test)
 train_data, train_labels = convert(input_data_2)
 
+# scaler = preprocessing.StandardScaler().fit(test_data)
 scaler = preprocessing.StandardScaler().fit(train_data)
 train_data = scaler.transform(train_data)
 test_data =  scaler.transform(test_data)
@@ -62,23 +64,40 @@ print(loss_and_metrics)
 
 # Online learning
 #==============================================================================
-# increment = 100
+# increment = 16
 # chunks_train_data = [train_data[x:x+increment] for x in range(0, len(train_data), increment)]
 # chunks_train_labels = [train_labels[x:x+increment] for x in range(0, len(train_labels), increment)]
 # 
 # test_accuracy = [];
+# test_loss = [];
+# train_accuracy = [];
+# train_loss = [];
 # for i, el in enumerate(chunks_train_data):
 #     print(i)
 #     train_loss_and_metrics = model.train_on_batch(el, chunks_train_labels[i])
 #     print(train_loss_and_metrics)
+#     train_loss.append(train_loss_and_metrics[0])
+#     train_accuracy.append(train_loss_and_metrics[1])
 #     test_loss_and_metrics = model.evaluate(test_data, test_labels, batch_size=128)
 #     print(test_loss_and_metrics)
+#     test_loss.append(test_loss_and_metrics[0])
 #     test_accuracy.append(test_loss_and_metrics[1])
 # 
-# plt.plot(test_accuracy)
-# plt.ylabel('Accuracy of test data')
-# plt.xlabel('Round number')
+# fig = plt.figure()
+# 
+# ax1 = fig.add_subplot(211)
+# ax1.plot(train_loss)
+# ax1.plot(test_loss)
+# ax1.set_ylabel('Loss')
+# ax1.set_xlabel('Round number')
+# ax1.legend(['training', 'testing'], loc='upper left')
+# 
+# ax2 = fig.add_subplot(212)
+# ax2.plot(train_accuracy)
+# ax2.plot(test_accuracy)
+# ax2.set_ylabel('Accuracy')
+# ax2.set_xlabel('Round number')
+# ax2.legend(['training', 'testing'], loc='upper left')
 # plt.show()
 #==============================================================================
-
 
--- ranking/online_processing.py
+++ ranking/online_processing.py
@@ -2,6 +2,7 @@
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 # Train data with DL model
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -60,17 +61,35 @@ chunks_train_data = [train_data[x:x+increment] for x in range(0, len(train_data)
 chunks_train_labels = [train_labels[x:x+increment] for x in range(0, len(train_labels), increment)]
 
 test_accuracy = [];
+test_loss = [];
+train_accuracy = [];
+train_loss = [];
 for i, el in enumerate(chunks_train_data):
     print(i)
     train_loss_and_metrics = model.train_on_batch(el, chunks_train_labels[i])
     print(train_loss_and_metrics)
+    train_loss.append(train_loss_and_metrics[0])
+    train_accuracy.append(train_loss_and_metrics[1])
     test_loss_and_metrics = model.evaluate(test_data, test_labels, batch_size=128)
     print(test_loss_and_metrics)
+    test_loss.append(test_loss_and_metrics[0])
     test_accuracy.append(test_loss_and_metrics[1])
 
-plt.plot(test_accuracy)
-plt.ylabel('Accuracy of test data')
-plt.xlabel('Round number')
+fig = plt.figure()
+
+ax1 = fig.add_subplot(211)
+ax1.plot(train_loss)
+ax1.plot(test_loss)
+ax1.set_ylabel('Loss')
+ax1.set_xlabel('Round number')
+ax1.legend(['training', 'testing'], loc='upper left')
+
+ax2 = fig.add_subplot(212)
+ax2.plot(train_accuracy)
+ax2.plot(test_accuracy)
+ax2.set_ylabel('Accuracy')
+ax2.set_xlabel('Round number')
+ax2.legend(['training', 'testing'], loc='upper left')
 plt.show()
 
 
